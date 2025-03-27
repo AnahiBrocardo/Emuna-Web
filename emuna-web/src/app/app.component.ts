60c58da093d24ae64debb98ed41d1ea1ba20104e
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-03-27 20:08:34 -0300

    Configuraciones

--- emuna-web/src/app/app.component.ts
+++ emuna-web/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { HomeComponent } from './pages/home/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'emuna-web';
+export class AppComponent implements OnInit{
+  title = 'Emuna crochet';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
