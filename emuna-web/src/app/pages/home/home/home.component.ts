8b2c8bdeceae9ad6e96645a664f953a3701a2e57
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-03-27 21:36:36 -0300

    Home page

--- emuna-web/src/app/pages/home/home/home.component.ts
+++ emuna-web/src/app/pages/home/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../components/navbar/navbar/navbar.component';
+import { AboutMeComponent } from '../../../components/about/about-me/about-me.component';
+import { CarruselComponent } from '../../../components/carrusel/carrusel/carrusel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, AboutMeComponent, CarruselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
