5a38e9d1b2d1a7e58949c6cccc74246946f469cc
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-04-01 13:36:06 -0300

    formulario contacto

--- emuna-web/src/app/components/contact/contact.component.ts
+++ emuna-web/src/app/components/contact/contact.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+   form= inject(FormBuilder);
+   
+   contactForm= this.form.group({
+    email: ['', [Validators.required, Validators.email]],
+    subject: ['', [Validators.required]],
+    message: ['', Validators.required]
+   })
 
+  send(){
+
+  }
 }
