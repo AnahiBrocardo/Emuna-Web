5d90708389a8432b5d4ec85d1ca2adfa719a63cf
Anahi Brocardo <anahibrocardo213@gmail.com>
2025-04-08 19:01:32 -0300

    Traduccion patrones

--- emuna-web/src/app/components/patterns/patterns.component.ts
+++ emuna-web/src/app/components/patterns/patterns.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslationService } from '../../services/translation.service';
+import { TranslateService } from '@ngx-translate/core';
+import { forkJoin, from } from 'rxjs';
+import { DomElementSchemaRegistry } from '@angular/compiler';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-patterns',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './patterns.component.html',
   styleUrl: './patterns.component.css'
 })
-export class PatternsComponent {
+export class PatternsComponent implements OnInit{
+  
+  title: string='';
+  download: string='';
+  note: string='';
+  freePatterns: any =[];
+  langSubscription: any;
 
+  constructor(private translationService: TranslationService,
+      private translate: TranslateService){}
+  
+  ngOnInit(): void {
+    this.loadPatterns();
+
+    this.langSubscription = this.translate.onLangChange.subscribe(() => {
+      this.loadPatterns();
+    });
+  }
+
+  loadPatterns(): void {
+        forkJoin([
+          from(this.translationService.translate('FreePatterns.title')),
+          from(this.translationService.translate('FreePatterns.download')),
+          from(this.translationService.translate('FreePatterns.note')),
+          from(this.translationService.translate('FreePatterns.items'))
+        ]).subscribe(([title, download, note, items]) => {
+          this.title = title;
+          this.download=download;
+          this.note= note;
+          for(let i=0; i<items.length; i++){
+           this.freePatterns[i]= items[i];
+          }
+        });
+  
+    }
 }
--- emuna-web/tailwind.config.js
+++ emuna-web/tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
     "./node_modules/flowbite/**/*.js"
   ],
+  theme: {
+    extend: {
+      colors: {
+        peach: {
+          DEFAULT: '#FFD89C',
+          50:  '#FFF4E3',
+          100: '#FFEBCF',
+          200: '#FFE0B8', 
+          300: '#FFD89C',
+          400: '#FFC676',
+        }
+      },
+    },
+  },
   plugins: [
     require('flowbite/plugin')
   ],
